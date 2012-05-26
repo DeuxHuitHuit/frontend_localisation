1558cf7120b062a6a71b488d7855fec01a1abd9d
Vlad Ghita <vlad_micutul@yahoo.com>
2012-05-26 20:53:10 +0300

    Fixed existing tabs filtering.

--- assets/frontend_localisation.multilingual_tabs.js
+++ assets/frontend_localisation.multilingual_tabs.js
@@ -1,57 +1,61 @@
 (function($, undefined){
 
 	var collection = {
-		$m_tabs : $(),
-		$m_panels : $()
+		$m_tabs: $(),
+		$m_panels: $()
 	};
 
-	$.fn.symphonyMultilingualTabs = function() {
+	$.fn.symphonyMultilingualTabs = function(){
 
-		return $(this).filter(':not(hasClass(multilingual-initialised))').each(function(){
+		return $(this)
+			.filter(function(){
+				return $(this).hasClass('multilingual-initialised') !== true;
+			})
+			.each(function(){
 
-			var $this = $(this).addClass('multilingual-initialised');
+				var $this = $(this).addClass('multilingual-initialised');
 
-			// safe checks
-			var $m_ul = $this.find('ul.tabs');
-			if( $m_ul.length === 0 ) return $this;
+				// safe checks
+				var $m_ul = $this.find('ul.tabs');
+				if( $m_ul.length === 0 ) return $this;
 
-			var $m_tabs = $m_ul.find('li');
-			if( $m_tabs.length === 0 ) return $this;
+				var $m_tabs = $m_ul.find('li');
+				if( $m_tabs.length === 0 ) return $this;
 
-			var $m_panels = $this.find('.tab-element, .tab-panel');
-			if( $m_panels.length === 0 ) return $this;
+				var $m_panels = $this.find('.tab-element, .tab-panel');
+				if( $m_panels.length === 0 ) return $this;
 
-			// add language information
-			$m_tabs.each(function(){
-				$(this).data('lang_code', $(this).attr('class'));
+				// add language information
+				$m_tabs.each(function(){
+					$(this).data('lang_code', $(this).attr('class'));
+				});
+
+				// current tab which will be clicked
+				var $current_tab = '';
+
+				if( collection.$m_tabs.length === 0 ){
+					$current_tab = $m_tabs.eq(0);
+				}
+				else{
+					$current_tab = collection.$m_tabs.filter('.active:eq(0)');
+				}
+
+				// store new elements
+				collection.$m_tabs = collection.$m_tabs.add($m_tabs);
+				collection.$m_panels = collection.$m_panels.add($m_panels);
+
+				// bind events
+				$m_ul.on('click', 'li', function(){
+					var lang_code = $(this).data('lang_code');
+
+					collection.$m_tabs.removeClass('active').filter('.'+lang_code).addClass('active');
+					collection.$m_panels.hide().filter('.tab-'+lang_code).show();
+				});
+
+				// trigger current tab
+				$current_tab.click();
+
 			});
-
-			// current tab which will be clicked
-			var $current_tab = '';
-
-			if( collection.$m_tabs.length === 0){
-				$current_tab = $m_tabs.eq(0);
-			}
-			else{
-				$current_tab = collection.$m_tabs.filter('.active:eq(0)');
-			}
-
-			// store new elements
-			collection.$m_tabs = collection.$m_tabs.add($m_tabs);
-			collection.$m_panels = collection.$m_panels.add($m_panels);
-
-			// bind events
-			$m_ul.on('click', 'li', function(){
-				var lang_code = $(this).data('lang_code');
-
-				collection.$m_tabs.removeClass('active').filter('.'+lang_code).addClass('active');
-				collection.$m_panels.hide().filter('.tab-'+lang_code).show();
-			});
-
-			// trigger current tab
-			$current_tab.click();
-
-		});
 
 	};
 
