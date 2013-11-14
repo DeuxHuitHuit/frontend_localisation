47d5ee6ae6a8ba1aa76444758a84325a1a64b0df
Vlad Ghita <vlad_micutul@yahoo.com>
2013-11-14 13:44:39 +0200

    Release 2.2

--- extension.driver.php
+++ extension.driver.php
@@ -1,11 +1,10 @@
 <?php
 
-	if( !defined( '__IN_SYMPHONY__' ) ) die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
+	if (!defined('__IN_SYMPHONY__')) {
+		die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
+	}
 
-
-
-	final class Extension_Frontend_Localisation extends Extension
-	{
+	final class Extension_Frontend_Localisation extends Extension {
 
 		private static $assets_loaded = false;
 
@@ -15,58 +14,58 @@
 		/*  Installation  */
 		/*------------------------------------------------------------------------------------------------*/
 
-		public function install(){
-			try{
+		public function install() {
+			try {
 				$this->meetDependencies();
-			} catch( Exception $e ){
-				Administration::instance()->Page->pageAlert( $e->getMessage(), Alert::ERROR );
+			} catch (Exception $e) {
+				Administration::instance()->Page->pageAlert($e->getMessage(), Alert::ERROR);
 			}
 
 			/* Configuration */
-			Symphony::Configuration()->set( 'langs', '', 'frontend_localisation' );
-			Symphony::Configuration()->set( 'main_lang', '', 'frontend_localisation' );
-			Symphony::Configuration()->set( 'ref_lang', '', 'frontend_localisation' );
+			Symphony::Configuration()->set('langs', '', 'frontend_localisation');
+			Symphony::Configuration()->set('main_lang', '', 'frontend_localisation');
+			Symphony::Configuration()->set('ref_lang', '', 'frontend_localisation');
 
 			Symphony::Configuration()->write();
 
 			return true;
 		}
 
-		public function uninstall(){
+		public function uninstall() {
 
 			/* Configuration */
-			Symphony::Configuration()->remove( 'frontend_localisation' );
+			Symphony::Configuration()->remove('frontend_localisation');
 			Symphony::Configuration()->write();
 
 			return true;
 		}
 
-		public function update($previousVersion = false){
-			try{
+		public function update($previousVersion = false) {
+			try {
 				$this->meetDependencies();
-			} catch( Exception $e ){
-				Administration::instance()->Page->pageAlert( $e->getMessage(), Alert::ERROR );
+			} catch (Exception $e) {
+				Administration::instance()->Page->pageAlert($e->getMessage(), Alert::ERROR);
 			}
 
-			if( version_compare( $previousVersion, '1.4', '<' ) ){
-				Symphony::Configuration()->remove( 'fl_driver', 'frontend_localisation' );
+			if (version_compare($previousVersion, '1.4', '<')) {
+				Symphony::Configuration()->remove('fl_driver', 'frontend_localisation');
 
-				Symphony::Configuration()->set( 'lang_codes', '', 'frontend_localisation' );
-				Symphony::Configuration()->set( 'main_lang', '', 'frontend_localisation' );
+				Symphony::Configuration()->set('lang_codes', '', 'frontend_localisation');
+				Symphony::Configuration()->set('main_lang', '', 'frontend_localisation');
 
-				$ref_lang = Symphony::Configuration()->get( 'reference_language', 'frontend_localisation' );
-				Symphony::Configuration()->set( 'ref_lang', $ref_lang, 'frontend_localisation' );
+				$ref_lang = Symphony::Configuration()->get('reference_language', 'frontend_localisation');
+				Symphony::Configuration()->set('ref_lang', $ref_lang, 'frontend_localisation');
 
-				$consolidate = Symphony::Configuration()->get( 'consolidate_translations', 'frontend_localisation' );
-				Symphony::Configuration()->set( 'consolidate', $consolidate, 'frontend_localisation' );
+				$consolidate = Symphony::Configuration()->get('consolidate_translations', 'frontend_localisation');
+				Symphony::Configuration()->set('consolidate', $consolidate, 'frontend_localisation');
 			}
 
-			if( version_compare( $previousVersion, '2.0', '<' ) ){
-				General::deleteDirectory( WORKSPACE.Symphony::Configuration()->get( 'translation_path', 'frontend_localisation' ) );
+			if (version_compare($previousVersion, '2.0', '<')) {
+				General::deleteDirectory(WORKSPACE . Symphony::Configuration()->get('translation_path', 'frontend_localisation'));
 
-				try{
-					Symphony::Database()->query( "ALTER TABLE `tbl_pages` DROP `translations`" );
-				} catch( Exception $e ){
+				try {
+					Symphony::Database()->query("ALTER TABLE `tbl_pages` DROP `translations`");
+				} catch (Exception $e) {
 				}
 			}
 		}
@@ -77,7 +76,7 @@
 		/*  Delegates  */
 		/*------------------------------------------------------------------------------------------------*/
 
-		public function getSubscribedDelegates(){
+		public function getSubscribedDelegates() {
 			return array(
 
 				array(
@@ -85,28 +84,21 @@
 					'delegate' => 'FrontendInitialised',
 					'callback' => 'dFrontendInitialised'
 				),
-
 				array(
 					'page'     => '/backend/',
 					'delegate' => 'InitaliseAdminPageHead',
 					'callback' => 'dInitialiseAdminPageHead'
 				),
-
-
-
 				array(
 					'page'     => '/frontend/',
 					'delegate' => 'FrontendParamsPostResolve',
 					'callback' => 'dFrontendParamsPostResolve'
 				),
-
-
 				array(
 					'page'     => '/system/preferences/',
 					'delegate' => 'AddCustomPreferenceFieldsets',
 					'callback' => 'dAddCustomPreferenceFieldsets'
 				),
-
 				array(
 					'page'     => '/system/preferences/',
 					'delegate' => 'Save',
@@ -124,26 +116,26 @@
 		/**
 		 * Frontend
 		 */
-		public function dFrontendInitialised(){
-			if( !$this->meetDependencies( true ) ){
+		public function dFrontendInitialised() {
+			if (!$this->meetDependencies(true)) {
 				return;
 			}
 
 			$this->_initFLang();
 
-			Lang::set( FLang::getLangCode() );
+			Lang::set(FLang::getLangCode());
 		}
 
-		public function dFrontendParamsPostResolve($context){
-			if( !$this->meetDependencies( true ) ){
+		public function dFrontendParamsPostResolve($context) {
+			if (!$this->meetDependencies(true)) {
 				return;
 			}
 
-			if( isset($context['params']['url-fl-language']) ){
+			if (isset($context['params']['url-fl-language'])) {
 				unset($context['params']['url-fl-language']);
 			}
 
-			if( isset($context['params']['url-fl-region']) ){
+			if (isset($context['params']['url-fl-region'])) {
 				unset($context['params']['url-fl-region']);
 			}
 		}
@@ -151,42 +143,42 @@
 		/**
 		 * Backend
 		 */
-		public function dInitialiseAdminPageHead(){
-			try{
+		public function dInitialiseAdminPageHead() {
+			try {
 				$this->meetDependencies();
-			} catch( Exception $e ){
-				Administration::instance()->Page->pageAlert( $e->getMessage(), Alert::ERROR );
+			} catch (Exception $e) {
+				Administration::instance()->Page->pageAlert($e->getMessage(), Alert::ERROR);
 			}
 
 			$this->_initFLang();
 		}
 
-		private function _initFLang(){
-			require_once(EXTENSIONS.'/frontend_localisation/lib/class.FLang.php');
+		private function _initFLang() {
+			require_once(EXTENSIONS . '/frontend_localisation/lib/class.FLang.php');
 
 			// initialize Language codes
-			$langs = Symphony::Configuration()->get( 'langs', 'frontend_localisation' );
-			FLang::setLangs( $langs );
+			$langs = Symphony::Configuration()->get('langs', 'frontend_localisation');
+			FLang::setLangs($langs);
 
 			// initialize Main language
-			$main_lang = Symphony::Configuration()->get( 'main_lang', 'frontend_localisation' );
-			if( !FLang::setMainLang( $main_lang ) ){
+			$main_lang = Symphony::Configuration()->get('main_lang', 'frontend_localisation');
+			if (!FLang::setMainLang($main_lang)) {
 				$langs = FLang::getLangs();
 
-				if( isset($langs[0]) && !FLang::setLangCode( $langs[0] ) ){
+				if (isset($langs[0]) && !FLang::setLangCode($langs[0])) {
 					// do something useful here if no lang is set ...
 				}
 			}
 
 			// read current language
-			$language = General::sanitize( (string) $_REQUEST['fl-language'] );
-			$region   = General::sanitize( (string) $_REQUEST['fl-region'] );
+			$language = General::sanitize((string) $_REQUEST['fl-language']);
+			$region   = General::sanitize((string) $_REQUEST['fl-region']);
 
 			// set language code
-			if( false === FLang::setLangCode( $language, $region ) ){
+			if (false === FLang::setLangCode($language, $region)) {
 
 				// language code is not supported, fallback to main lang
-				if( false === FLang::setLangCode( FLang::getMainLang() ) ){
+				if (false === FLang::setLangCode(FLang::getMainLang())) {
 					// do something useful here if no lang is set ...
 				}
 			}
@@ -203,23 +195,23 @@
 		 *
 		 * @param array $context
 		 */
-		public function dAddCustomPreferenceFieldsets(array $context){
-			if( !$this->meetDependencies( true ) ){
+		public function dAddCustomPreferenceFieldsets(array $context) {
+			if (!$this->meetDependencies(true)) {
 				return;
 			}
 
-			Administration::instance()->Page->addScriptToHead( URL.'/extensions/frontend_localisation/assets/frontend_localisation.preferences.js', null, false );
+			Administration::instance()->Page->addScriptToHead(URL . '/extensions/frontend_localisation/assets/frontend_localisation.preferences.js', null, false);
 
 			$group = new XMLElement('fieldset');
-			$group->setAttribute( 'class', 'settings' );
-			$group->appendChild( new XMLElement('legend', __( 'Frontend Localisation' )) );
+			$group->setAttribute('class', 'settings');
+			$group->appendChild(new XMLElement('legend', __('Frontend Localisation')));
 
 			$div = new XMLElement('div', null, array('class' => 'two columns'));
-			$this->_appendLangs( $div, $context );
-			$this->_appendMainLang( $div, $context );
-			$group->appendChild( $div );
+			$this->_appendLangs($div, $context);
+			$this->_appendMainLang($div, $context);
+			$group->appendChild($div);
 
-			$context['wrapper']->appendChild( $group );
+			$context['wrapper']->appendChild($group);
 		}
 
 		/**
@@ -228,21 +220,21 @@
 		 * @param XMLElement &$wrapper
 		 * @param array      $context
 		 */
-		private function _appendLangs(&$wrapper, $context){
-			$label = Widget::Label( __( 'Site languages' ), null, 'column', 'frontend_localisation'.'_langs' );
+		private function _appendLangs(&$wrapper, $context) {
+			$label = Widget::Label(__('Site languages'), null, 'column', 'frontend_localisation' . '_langs');
 
-			require_once EXTENSIONS.'/languages/extension.driver.php';
+			require_once EXTENSIONS . '/languages/extension.driver.php';
 
-			$options = Extension_Languages::findOptions( FLang::getLangs() );
+			$options = Extension_Languages::findOptions(FLang::getLangs());
 
-			$label->appendChild( Widget::Select( 'settings['.'frontend_localisation'.'][langs]', $options, array('multiple' => 'multiple') ) );
-			$label->appendChild( new XMLElement('p', __( 'Select languages of the site.' ), array('class' => 'help')) );
+			$label->appendChild(Widget::Select('settings[' . 'frontend_localisation' . '][langs]', $options, array('multiple' => 'multiple')));
+			$label->appendChild(new XMLElement('p', __('Select languages of the site.'), array('class' => 'help')));
 
-			if( isset($context['errors']['frontend_localisation']['langs']) ){
-				$wrapper->appendChild( Widget::Error( $label, $context['errors']['frontend_localisation']['langs'] ) );
+			if (isset($context['errors']['frontend_localisation']['langs'])) {
+				$wrapper->appendChild(Widget::Error($label, $context['errors']['frontend_localisation']['langs']));
 			}
-			else{
-				$wrapper->appendChild( $label );
+			else {
+				$wrapper->appendChild($label);
 			}
 		}
 
@@ -252,24 +244,24 @@
 		 * @param XMLElement &$wrapper
 		 * @param array      $context
 		 */
-		private function _appendMainLang(&$wrapper, $context){
-			$label         = Widget::Label( __( 'Main language' ), null, 'column', 'frontend_localisation'.'_main_lang' );
+		private function _appendMainLang(&$wrapper, $context) {
+			$label         = Widget::Label(__('Main language'), null, 'column', 'frontend_localisation' . '_main_lang');
 			$main_lang     = FLang::getMainLang();
 			$all_languages = Languages::local()->listAll();
 
 			$options = array();
-			foreach(FLang::getLangs() as $lang_code){
+			foreach (FLang::getLangs() as $lang_code) {
 				$options[] = array($lang_code, ($lang_code == $main_lang), $all_languages[$lang_code]);
 			}
 
-			$label->appendChild( Widget::Select( 'settings['.'frontend_localisation'.'][main_lang]', $options ) );
-			$label->appendChild( new XMLElement('p', __( 'Select the main language of the site.' ), array('class' => 'help')) );
+			$label->appendChild(Widget::Select('settings[' . 'frontend_localisation' . '][main_lang]', $options));
+			$label->appendChild(new XMLElement('p', __('Select the main language of the site.'), array('class' => 'help')));
 
-			if( isset($context['errors']['frontend_localisation']['main_lang']) ){
-				$wrapper->appendChild( Widget::Error( $label, $context['errors']['frontend_localisation']['main_lang'] ) );
+			if (isset($context['errors']['frontend_localisation']['main_lang'])) {
+				$wrapper->appendChild(Widget::Error($label, $context['errors']['frontend_localisation']['main_lang']));
 			}
-			else{
-				$wrapper->appendChild( $label );
+			else {
+				$wrapper->appendChild($label);
 			}
 		}
 
@@ -280,27 +272,26 @@
 		 *
 		 * @return boolean
 		 */
-		public function dSave(array $context){
+		public function dSave(array $context) {
 			$valid = true;
 
 			$data   = & $context['settings']['frontend_localisation'];
-			$errors = & $context['errors']['frontend_localisation'];
+			$errors = array();
 
-			if( !$this->meetDependencies( true ) ){
+			if (!$this->meetDependencies(true)) {
 				return $valid;
 			}
 
 			/* Language codes */
 
 			$old_langs = FLang::getLangs();
-			if( !FLang::setLangs( $data['langs'] ) ){
+			if (!FLang::setLangs($data['langs'])) {
 				$valid           = false;
-				$errors['langs'] = __( 'Please fill at least one valid language code.' );
+				$errors['langs'] = __('Please fill at least one valid language code.');
 			}
 			$new_langs = FLang::getLangs();
-			unset($context['settings']['frontend_localisation']['langs']);
-			Symphony::Configuration()->set( 'langs', implode( ',', $new_langs ), 'frontend_localisation' );
-
+			unset($data['langs']);
+			Symphony::Configuration()->set('langs', implode(',', $new_langs), 'frontend_localisation');
 
 			/**
 			 * When saving Preferences, supplies the old_languages and new_languages arrays.
@@ -313,28 +304,30 @@
 			 * @param array  $old_langs - old language codes
 			 * @param array  $new_langs - new language codes
 			 */
-			Symphony::ExtensionManager()->notifyMembers( 'FLSavePreferences', '/extensions/frontend_localisation/', array(
+			Symphony::ExtensionManager()->notifyMembers('FLSavePreferences', '/extensions/frontend_localisation/', array(
 				'context'   => $context,
 				'old_lang'  => $old_langs,
 				'new_langs' => $new_langs
-			) );
-
+			));
 
 			/* Main language */
 
-			if( !FLang::setMainLang( $data['main_lang'] ) ){
+			if (!FLang::setMainLang($data['main_lang'])) {
 
-				if( !empty($old_langs) || !(isset($new_langs[0]) && FLang::setMainLang( $new_langs[0] )) ){
+				if (!empty($old_langs) || !(isset($new_langs[0]) && FLang::setMainLang($new_langs[0]))) {
 					$valid               = false;
-					$errors['main_lang'] = __( 'Invalid language code.' );
+					$errors['main_lang'] = __('Invalid language code.');
 				}
 			}
 			$main_lang = FLang::getMainLang();
-			unset($context['settings']['frontend_localisation']['main_lang']);
-			Symphony::Configuration()->set( 'main_lang', $main_lang, 'frontend_localisation' );
-
+			unset($data['main_lang']);
+			Symphony::Configuration()->set('main_lang', $main_lang, 'frontend_localisation');
 
 			Symphony::Configuration()->write();
+
+			if (!empty($errors)) {
+				$context['errors']['frontend_localisation'] = $errors;
+			}
 
 			return $valid;
 		}
@@ -345,19 +338,19 @@
 		/*  Public utilities  */
 		/*------------------------------------------------------------------------------------------------*/
 
-		public static function appendAssets(){
-			if( self::$assets_loaded === false
-				&& class_exists( 'Administration' )
+		public static function appendAssets() {
+			if (self::$assets_loaded === false
+				&& class_exists('Administration')
 				&& Administration::instance() instanceof Administration
 				&& Administration::instance()->Page instanceof HTMLPage
-			){
+			) {
 				self::$assets_loaded = true;
 
 				$page = Administration::instance()->Page;
 
-				$page->addStylesheetToHead( URL.'/extensions/frontend_localisation/assets/frontend_localisation.multilingual_tabs.css', 'screen', null, false );
-				$page->addScriptToHead( URL.'/extensions/frontend_localisation/assets/frontend_localisation.multilingual_tabs.js', null, false );
-				$page->addScriptToHead( URL.'/extensions/frontend_localisation/assets/frontend_localisation.multilingual_tabs_init.js', null, false );
+				$page->addStylesheetToHead(URL . '/extensions/frontend_localisation/assets/frontend_localisation.multilingual_tabs.css', 'screen', null, false);
+				$page->addScriptToHead(URL . '/extensions/frontend_localisation/assets/frontend_localisation.multilingual_tabs.js', null, false);
+				$page->addScriptToHead(URL . '/extensions/frontend_localisation/assets/frontend_localisation.multilingual_tabs_init.js', null, false);
 			}
 		}
 
@@ -370,22 +363,21 @@
 		 *
 		 * @return bool
 		 */
-		public function meetDependencies($return_status = false){
+		public function meetDependencies($return_status = false) {
 
 			// depends on "Languages"
-			$languages_status = ExtensionManager::fetchStatus( array('handle' => 'languages') );
-			$languages_status = current( $languages_status );
+			$languages_status = ExtensionManager::fetchStatus(array('handle' => 'languages'));
+			$languages_status = current($languages_status);
 
-			if( $languages_status != EXTENSION_ENABLED ){
-				if( $return_status ){
+			if ($languages_status != EXTENSION_ENABLED) {
+				if ($return_status) {
 					return false;
 				}
-				else{
+				else {
 					throw new Exception('Frontend Localisation depends on Languages extension.');
 				}
 			}
 
 			return true;
 		}
-
 	}
