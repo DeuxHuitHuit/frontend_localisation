0f6b0ef2cd9264e37c02d632066aff3c5bdebd73
Vlad Ghita <vlad_micutul@yahoo.com>
2013-11-26 16:27:43 +0200

    Use main language as first active tab. Always.

--- assets/frontend_localisation.multilingual_tabs.js
+++ assets/frontend_localisation.multilingual_tabs.js
@@ -1,45 +1,38 @@
-(function($, undefined){
+(function ($, undefined) {
 
 	var collection = {
 		$m_fields: $(),
-		$m_tabs: $(),
+		$m_tabs:   $(),
 		$m_panels: $()
 	};
 
-	$.fn.symphonyMultilingualTabs = function(){
+	$.fn.symphonyMultilingualTabs = function () {
 
-		return $(this).not(collection.$m_fields).each(function(){
+		return $(this).not(collection.$m_fields).each(function () {
 
 			var $this = $(this);
 
 			// safe checks
 			var $m_ul = $this.find('ul.tabs');
-			if( $m_ul.length === 0 ) return $this;
+			if ($m_ul.length === 0) return $this;
 
 			var $m_tabs = $m_ul.find('li');
-			if( $m_tabs.length === 0 ) return $this;
+			if ($m_tabs.length === 0) return $this;
 
 			var $m_panels = $this.find('.tab-element, .tab-panel');
-			if( $m_panels.length === 0 ) return $this;
+			if ($m_panels.length === 0) return $this;
 
 			// add language information
-			$m_tabs.each(function(){
+			$m_tabs.each(function () {
 				$(this).data('lang_code', $(this).attr('class'));
 			});
 
 			// current tab which will be clicked
 			var $current_tab = '';
-			
-			//current language defined by symphony in the html header with lang attr
-			var currentlanguage = jQuery('html').attr('lang');
 
-			if( collection.$m_tabs.length === 0 ){
+			if (collection.$m_tabs.length === 0) {
 				$current_tab = $m_tabs.eq(0);
-			}
-			else if (currentlanguage.length > 0){
-				$current_tab = collection.$m_tabs.filter('.'+currentlanguage+'');
-			}
-			else{
+			} else {
 				$current_tab = collection.$m_tabs.filter('.active:eq(0)');
 			}
 
@@ -49,11 +42,11 @@
 			collection.$m_panels = collection.$m_panels.add($m_panels);
 
 			// bind events
-			$m_ul.on('click', 'li', function(){
+			$m_ul.on('click', 'li', function () {
 				var lang_code = $(this).data('lang_code');
 
-				collection.$m_tabs.removeClass('active').filter('.'+lang_code).addClass('active');
-				collection.$m_panels.hide().filter('.tab-'+lang_code).show();
+				collection.$m_tabs.removeClass('active').filter('.' + lang_code).addClass('active');
+				collection.$m_panels.hide().filter('.tab-' + lang_code).show();
 			});
 
 			// trigger current tab
